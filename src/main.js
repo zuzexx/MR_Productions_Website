d39307c5a52046c5c4e99402c59cdcf908baa895
zuzexx <tjasa.zilavec@gmail.com>
2022-09-15 17:07:11 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
-import router from "./router/router.js";
 
-createApp(App).mount("#app").$forceUpdate(router);
+createApp(App).mount("#app");
--- src/routes/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p>this is home</p>
-</template>
-
-<script>
-export default {
-  name: "Home",
-};
-</script>
