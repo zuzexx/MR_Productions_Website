daedf6524c11073e6afb3d566a90b8e114fd04ea
zuzexx <tjasa.zilavec@gmail.com>
2022-09-17 11:36:28 +0200

    .

--- src/components/Home/Grid.vue
+++ src/components/Home/Grid.vue
@@ -5,7 +5,7 @@
     <div
       v-for="grid in grids"
       :key="grid"
-      class="flex flex-col bg-zinc-900 mt-12 text-center w-11/12 md:w-1/3 mx-3 max-w-lg xl:mx-6 hover:cursor-pointer hover:shadow-lg hover:-translate-y-1 transition-all duration-150 motion-safe:animate-fadeIn"
+      class="flex flex-col bg-zinc-900 mt-12 text-center w-11/12 md:w-1/3 mx-3 max-w-lg xl:mx-6 hover:cursor-pointer hover:shadow-lg hover:-translate-y-1 transition-all duration-150"
     >
       <img :src="grid.image" alt="image" />
       <h3
