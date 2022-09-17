2e98984b59d667d1f9378262084aff25c8c746a4
zuzexx <tjasa.zilavec@gmail.com>
2022-09-17 12:31:22 +0200

    .animations done for main page

--- src/components/Home/Main.vue
+++ src/components/Home/Main.vue
@@ -3,11 +3,13 @@
     <article class="text-center flex flex-col justify-center">
       <h2
         class="text-zinc-300 uppercase font-medium ms:text-lg lg:text-xl xl:text-2xl"
+        x-intersect="$el.classList.add('fadeLeftRight')"
       >
         Capturing moments as they happen
       </h2>
       <p
         class="mt-8 text-zinc-500 leading-6 w-4/6 mx-auto text-xs sm:text-sm sm:leading-8 xl:text-lg lg:w-1/3"
+        x-intersect="$el.classList.add('fadeRightLeft')"
       >
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod sit ipsum
         impedit voluptas magnam sunt voluptates totam obcaecati eos. Eligendi
