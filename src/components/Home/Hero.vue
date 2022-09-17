8a27755f8d08017f120fa0e93d2224781d108a77
zuzexx <tjasa.zilavec@gmail.com>
2022-09-17 10:24:20 +0200

    .tested the hero section

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -2,19 +2,24 @@
   <section class="flex flex-col items-center h-auto text-zinc-300">
     <div
       class="background w-full h-screen flex flex-col justify-center items-center"
+      data-test="background-image-test"
     >
       <div
         class="mx-auto w-auto justify-center items-center text-center space-y-5 lg:space-y-10"
       >
-        <h2 class="font-semibold text-lg uppercase lg:text-3xl sm:text-xl">
+        <h2
+          class="font-semibold text-lg uppercase lg:text-3xl sm:text-xl"
+          data-test="small-header-test"
+        >
           - We take time to -
         </h2>
         <h1
           class="font-bold text-3xl uppercase sm:text-5xl lg:text-7xl font-['Archivo']"
+          data-test="big-header-test"
         >
           stand & stare
         </h1>
-        <Button text="BE INSPIRED" type="primary" />
+        <Button text="BE INSPIRED" type="primary" data-test="button-test" />
       </div>
     </div>
     <!--<img src="/Hero/hero.jpg" alt="" />-->
--- tests/unit/components/Home/Hero.test.js
+++ tests/unit/components/Home/Hero.test.js
@@ -1,2 +1,44 @@
 import { shallowMount } from "@vue/test-utils";
-import Hero from
+import Hero from "@/components/Home/Hero";
+
+let wrapper;
+beforeEach(() => {
+  wrapper = shallowMount(Hero);
+});
+//has background image
+//has appropriate h2
+//has appropriate h1
+//has button
+
+describe("Hero", () => {
+  it("has a background image", () => {
+    const background = wrapper.findAll('[data-test="background-image-test"]');
+    expect(background.length).toBe(1);
+  });
+  it("Displays background image", () => {
+    const background = wrapper.find('[data-test="background-image-test"]');
+    expect(background.classes()).toContain("background");
+  });
+  it("has a small header", () => {
+    const smallHeader = wrapper.find("[data-test='small-header-test']");
+    expect(smallHeader.exists()).toBe(true);
+  });
+  it("displays appropriate small header text", () => {
+    const smallHeader = wrapper.findAll("[data-test='small-header-test']");
+    const smallHeaderText = smallHeader.map((item) => item.text());
+    expect(smallHeaderText).toEqual(["- We take time to -"]);
+  });
+  it("has big header", () => {
+    const bigHeader = wrapper.find("[data-test='big-header-test']");
+    expect(bigHeader.exists()).toBe(true);
+  });
+  it("displays appropriate text in big header", () => {
+    const bigHeader = wrapper.findAll("[data-test='big-header-test']");
+    const bigHeaderText = bigHeader.map((item) => item.text());
+    expect(bigHeaderText).toEqual(["stand & stare"]);
+  });
+  it("has a button", () => {
+    const button = wrapper.find("[data-test='button-test']");
+    expect(button.exists()).toBe(true);
+  });
+});
