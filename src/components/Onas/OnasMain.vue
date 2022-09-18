7a391be2bc7143f34ccc0182f668aa8533aa0505
zuzexx <tjasa.zilavec@gmail.com>
2022-09-18 08:43:22 +0200

    .finished onasview and tested

--- src/components/Onas/OnasMain.vue
+++ src/components/Onas/OnasMain.vue
@@ -19,6 +19,37 @@
         culpa?
       </p>
     </article>
+    <article class="pt-32">
+      <div
+        class="w-4/6 flex flex-col justify-center mx-auto text-left lg:flex-row lg:w-10/12"
+      >
+        <img
+          src="/Onas/main-onas.jpg"
+          alt=" "
+          class="w-5/6 mx-auto lg:w-2/4 h-auto"
+          data-test="about-us-picture"
+        />
+        <div class="flex flex-col">
+          <h2
+            class="text-zinc-300 uppercase font-medium ms:text-lg lg:text-xl xl:text-2xl mx-auto mt-16 lg:mt-0"
+            data-test="onas-small-header-test"
+            x-intersect="$el.classList.add('fadeLeftRight')"
+          >
+            Capturing moments as they happen
+          </h2>
+          <p
+            class="mt-8 text-zinc-500 leading-6 w-5/6 mx-auto text-xs sm:text-sm sm:leading-8 xl:text-lg"
+            data-test="onas-paragraph-test"
+            x-intersect="$el.classList.add('fadeRightLeft')"
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod sit
+            ipsum impedit voluptas magnam sunt voluptates totam obcaecati eos.
+            Eligendi minus accusantium, nisi omnis nesciunt accusamus odit
+            inventore commodi culpa?
+          </p>
+        </div>
+      </div>
+    </article>
   </section>
 </template>
 
--- tests/unit/components/Onas/OnasMain.test.js
+++ tests/unit/components/Onas/OnasMain.test.js
@@ -1,18 +1,22 @@
 import { shallowMount } from "@vue/test-utils";
 import OnasMain from "@/components/Onas/OnasMain.vue";
 
-let wrapper, smallHeader, paragraph;
+let wrapper, smallHeader, paragraph, picture;
 beforeEach(() => {
   wrapper = shallowMount(OnasMain);
   smallHeader = wrapper.findAll("[data-test='onas-small-header-test']");
   paragraph = wrapper.findAll("[data-test='onas-paragraph-test']");
+  picture = wrapper.find("[data-test='about-us-picture']");
 });
 
 describe("OnasMain", () => {
   it("has a small header", () => {
-    expect(smallHeader.length).toBe(1);
+    expect(smallHeader.length).toBe(2);
   });
   it("has a paragraph", () => {
-    expect(paragraph.length).toBe(1);
+    expect(paragraph.length).toBe(2);
+  });
+  it("has a picture", () => {
+    expect(picture.exists()).toBe(true);
   });
 });
